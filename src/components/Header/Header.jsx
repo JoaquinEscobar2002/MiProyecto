8535b765794256930e339e59a54fb2aa83d86b90
Joaquin Escobar <escobarjoaquinc@gmail.com>
2023-07-10 02:04:43 -0300

    update primera pre-entrega

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css'
+import NavBar from '../NavBar/NavBar'
+
+const Header = () => {
+
+    return (
+        <header>
+            <NavBar />
+        </header>
+        
+        )
+    
+
+};
+
+export default Header; 
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './NavBar.css'
+
+const NavBar = () => {
+
+    return (
+        <>
+            <div>MiLogo</div>
+            <ul>
+                <li>Home</li>
+                <li>Catalogo</li>
+                <li>Como comprar</li>
+                <li>Contacto</li>
+            </ul>
+        </>
+        )
+    
+
+};
+
+export default NavBar;
